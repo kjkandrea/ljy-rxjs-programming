0cbcbbdd1d4ab156b48f1fd7de842376b3fcd993
andrea <kjkandrea@gmail.com>
2021-11-08 21:46:01 +0900

    Add unsubscribe

--- 02_basic/Observable.ts
+++ 02_basic/Observable.ts
@@ -11,9 +11,12 @@ const observable = new Observable(subscriber => {
 });
 
 console.log('just before subscribe');
-observable.subscribe({
+const subscription = observable.subscribe({
 	next(x) { console.log('got value ' + x); },
 	error(err) { console.error('something wrong occurred: ' + err); },
 	complete() { console.log('done'); }
 });
 console.log('just after subscribe');
+// Later:
+subscription.unsubscribe();
+console.log('unsubscribed')
