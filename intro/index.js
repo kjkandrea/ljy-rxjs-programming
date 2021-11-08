313cfca58d7a208d3d2d496e6a476772663a56a7
andrea <kjkandrea@gmail.com>
2021-11-08 20:50:23 +0900

    Add rxjs first logic

--- intro/index.js
+++ intro/index.js
@@ -1,3 +1,8 @@
 import { range } from 'rxjs'
+import { filter, map, scan } from 'rxjs/operators'
 
-console.log(range(1, 10))
+range(0, 10).pipe(
+  filter(x => x % 2 === 0),
+  map(x => x + x),
+  scan((acc, x) => acc + x, 0)
+).subscribe(console.log)
