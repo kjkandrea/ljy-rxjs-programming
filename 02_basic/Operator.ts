3dd9d241347402796dbe027685d24469dce39d48
andrea <kjkandrea@gmail.com>
2021-11-10 20:34:24 +0900

    Use operator ( filter )

--- 02_basic/Operator.ts
+++ 02_basic/Operator.ts
@@ -1,3 +1,11 @@
 import { interval, filter } from "rxjs";
 
-interval(700).subscribe(console.log)
+let count = 1 // 약수
+setInterval(
+	() => count = count + 1,
+	500
+)
+
+interval(700).pipe(
+	filter(v => v % 2 === 0)
+).subscribe(console.log)
