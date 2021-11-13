c0ebb811403d3b8752472fc3114c3ddbd939dc48
andrea <kjkandrea@gmail.com>
2021-11-13 14:29:26 +0900

    Add MergeMap

--- 05_conversionOperator/MergeMap.ts
+++ 05_conversionOperator/MergeMap.ts
@@ -8,5 +8,19 @@ const requests = [
 
 // from(requests).subscribe(x => x.subscribe(console.log)) // bad
 
-from(requests).pipe(mergeMap(v => v)).subscribe(console.log) // good?
+// from(requests).pipe(mergeMap(v => v)).subscribe(console.log) // good?
 
+const getPromise = (wait: number, value: any) => new Promise(res => setTimeout(() => res(value), wait))
+
+const requests2 = [
+	getPromise(200, { data: 1 }),
+	getPromise(400, { data: 2 }),
+	getPromise(300, { data: 3 }),
+	getPromise(100, { data: 4 }),
+	getPromise(500, { data: 5 }),
+	getPromise(600, { data: 5 }),
+]
+
+// from(requests2).pipe(mergeMap(v => v)).subscribe(console.log)
+
+from(requests2).pipe(mergeMap(v => v, 2)).subscribe(console.log) // 겁나어렵 ㅋㅋ 여튼 2개씩 수행됨
