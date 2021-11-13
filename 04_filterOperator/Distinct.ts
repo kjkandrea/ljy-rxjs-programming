9513db31b5031c148193fc8c154d6e937ce235a6
andrea <kjkandrea@gmail.com>
2021-11-13 13:27:32 +0900

    Add distinct depth

--- 04_filterOperator/Distinct.ts
+++ 04_filterOperator/Distinct.ts
@@ -4,3 +4,14 @@ import { from, distinct } from "rxjs";
 from([3,1,2,1,2,3,3,3,4,6,5,5,5,1,2]).pipe(
 	distinct()
 ).subscribe(console.log)
+
+from([
+	{ id: 1, name: 'JK'},
+	{ id: 2, name: 'SJ'},
+	{ id: 3, name: 'SJ'},
+	{ id: 4, name: 'JK'},
+	{ id: 5, name: 'MM'},
+	{ id: 6, name: 'JK'},
+]).pipe(
+	distinct(member => member.name)
+).subscribe(console.log)
