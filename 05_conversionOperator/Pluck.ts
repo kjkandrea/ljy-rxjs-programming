7258f424856e098dd3a6af91b0b9d3f918b2d8ea
andrea <kjkandrea@gmail.com>
2021-11-13 14:00:12 +0900

    Add Pluck

--- 05_conversionOperator/Pluck.ts
+++ 05_conversionOperator/Pluck.ts
@@ -1,15 +1,44 @@
-import { from, distinct, pluck } from "rxjs";
+import {from, distinct, pluck} from "rxjs";
 
 const users = [
-	{ id: 1, name: 'JK'},
-	{ id: 2, name: 'SJ'},
-	{ id: 3, name: 'SJ'},
-	{ id: 4, name: 'JK'},
-	{ id: 5, name: 'MM'},
-	{ id: 6, name: 'JK'},
+	{
+		id: 1, name: 'JK', detail: {
+			age: 30
+		}
+	},
+	{
+		id: 2, name: 'SJ', detail: {
+			age: 5
+		}
+	},
+	{
+		id: 3, name: 'SJ', detail: {
+			age: 20
+		}
+	},
+	{
+		id: 4, name: 'JK', detail: {
+			age: 28
+		}
+	},
+	{
+		id: 5, name: 'MM', detail: {
+			age: 2
+		}
+	},
+	{
+		id: 6, name: 'JK', detail: {
+			age: 32
+		}
+	},
 ]
 
 from(users).pipe(
 	distinct(user => user.name),
 	pluck('name')
 ).subscribe(console.log)
+
+from(users).pipe(
+	distinct(user => user.name),
+	pluck('detail', 'age')
+).subscribe(e => console.log(e))
